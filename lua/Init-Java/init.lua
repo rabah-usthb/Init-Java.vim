251ce882afeb021a62830943a89154a1b922148c
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-09-15 19:13:55 +0100

    add print statment inside setWindow for debugging

--- lua/Init-Java/init.lua
+++ lua/Init-Java/init.lua
@@ -20,6 +20,11 @@ end
 
 --set the floating wibdow
 function  M.setWindow(x,y,width,height,title)
+    print("x: ", x)
+    print("y: ", y)
+    print("width: ", width)
+    print("height: ", height)
+
     local win = floating.floating_buf({
     title = title,
     rect = {
