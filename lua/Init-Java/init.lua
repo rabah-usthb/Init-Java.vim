30211a342b99dee25f6982bea1475412af3832cd
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-09-20 00:19:18 +0100

    debugging

--- lua/Init-Java/init.lua
+++ lua/Init-Java/init.lua
@@ -333,16 +333,13 @@ local function check_and_unshift()
 
         -- If the character isn't `|`, it means it has been shifted or changed
         if charAtCol == "|" then
-            -- Unshift or undo the last action
-            print("No shift detected, character is still `|`")
-        else
 
             vim.api.nvim_input("<Esc>u")  -- Undo and return to insert mode
-            print("Unshifted: character at restricted column was changed")
+
         end
 end
 
-vim.api.nvim_create_autocmd("TextChangedP", {
+vim.api.nvim_create_autocmd("TextChangedI", {
     group = vim.api.nvim_create_augroup("ColumnShiftGroup", { clear = true }),
     callback = function()
         check_and_unshift()
