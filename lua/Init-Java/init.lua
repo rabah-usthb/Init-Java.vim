8999aead10e715143969539a10c32386d586f7d5
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-09-20 01:46:33 +0100

    debugging

--- lua/Init-Java/init.lua
+++ lua/Init-Java/init.lua
@@ -327,7 +327,7 @@ local function check_and_unshift()
 
         local charAtCol = line:sub(63,65)
         print("Pipe",charAtCol)
-
+       charAtCol = charAtCol:gsub("%s+","")
        if charAtCol ~= "|" then
 
         vim.api.nvim_input("<Esc>u")  -- Undo and return to insert mode
