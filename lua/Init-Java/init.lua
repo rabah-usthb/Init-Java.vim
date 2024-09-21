e484dba0f8841597d7f787c6f823fdd06a6d8044
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-09-21 22:46:40 +0100

    debugging

--- lua/Init-Java/init.lua
+++ lua/Init-Java/init.lua
@@ -265,7 +265,7 @@ function M.unshiftPipe()
      
   local line = vim.api.nvim_get_current_line()
   line = string.gsub(line,"│","")
-  line = string.sub(line,1,24).."│"..string.sub(line,26,61).."│"..string.sub(line,63,#line)
+  line = string.sub(line,1,24).."│"..string.sub(line,26,65).."│"
   vim.api.nvim_set_current_line(line)
 end
 
